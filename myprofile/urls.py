a97501b1a74c0574be757fd6308e70ae65083086
Rubyroy12 <you@example.com>
2021-07-10 02:54:07 +0300

    check first tempate

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 
 urlpatterns=[
-      url(r'^$',views.home,name='home')
-  
+    url(r'',views.home,name='home'),
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
